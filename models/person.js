ac9310e311f21f61e71d2b59aea48e8f3089deb3
aryanshivhare62 <168353237+aryanshivhare62@users.noreply.github.com>
2025-05-23 16:35:59 +0530

    created mongoDB server

--- models/person.js
+++ models/person.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 
 //Define person schema
 const personSchema = new mongoose.Schema({
@@ -26,9 +28,47 @@ const personSchema = new mongoose.Schema({
     salary: {
         type: Number,
         required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
     }
 })
 
+personSchema.pre('save', async function(next){
+    const person = this;
+
+    // hash the password olny if it is modified (or its new)
+    if(!person.isModified('password')) return next();
+    try{
+        const salt = await bcrypt.genSalt(10);
+        // hash password generate
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+
+        person.password = hashedPassword;
+
+        next();
+    }catch(err){
+        console.log(err);
+        next();
+    }
+})
+
+personSchema.methods.comparePassword = async function(candidatePassword) {
+    try{
+        // use bcrypt to compare provided password and hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }
+    catch(err){
+        throw err;
+    }
+};
 
 
 // create person model
--- server.js
+++ server.js
@@ -6,13 +6,48 @@ const db = require('./db')
 require('dotenv').config();
 
 const bodyParser = require('body-parser');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 app.use(bodyParser.json()); // req.body
 
 const Book = require('./models/book');
 const Person = require('./models/person');
 const Menu = require('./models/menu');
 
-app.get('/', (req, res) => {
+// middleware functions
+const logRequest = (req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+  next(); // Move on to the next phase
+};
+
+app.use(logRequest);
+
+passport.use(new LocalStrategy(async (USERNAME, password, done) => {
+  // authentication logic here
+  try{
+    const user = await Person.findOne({username: USERNAME});
+
+    if(!user){
+      return done(null, false, {message: 'Incorrect username'});
+    }
+    const isPasswordMatch = await user.comparePassword(password);
+    if(isPasswordMatch){
+      return done(null, user);
+    }else{
+      return done(null, false, {message: 'Incorrect password'});
+    }
+
+  }catch(err){
+    return done(err);
+  }
+}));
+
+app.use(passport.initialize());
+
+const localAuthMiddleware = passport.authenticate('local', {session: false});
+app.get('/', localAuthMiddleware, (req, res) => {
   res.send('Hello World')
 })
 
@@ -50,22 +85,24 @@ app.post('/menu', async (req, res) => {
   
 })
 
-app.get('/person/:workType', async(req, res) => {
-  try{
-    const workType = req.params.workType;
-    if(workType == 'chef' || workType == 'manager' || workType == 'waiter'){
+// app.get('/person/:workType', async(req, res) => {
+//   try{
+//     const workType = req.params.workType;
+//     if(workType == 'chef' || workType == 'manager' || workType == 'waiter'){
 
-      const response = await Person.find({work: workType});
-      console.log('data fatched');
-      res.status(200).json(response);
-    }else{
-      res.status(404).json({error: 'Invalid work type'});
-    }
-  }catch(err){
-    console.log(err);
-    res.status(500).json({error: 'Internal server error'});
-  }
-})
+//       const response = await Person.find({work: workType});
+//       console.log('data fatched');
+//       res.status(200).json(response);
+//     }else{
+//       res.status(404).json({error: 'Invalid work type'});
+//     }
+//   }catch(err){
+//     console.log(err);
+//     res.status(500).json({error: 'Internal server error'});
+//   }
+// })
+
+
 
 const personRoutes = require('./routes/personRoutes');
 
